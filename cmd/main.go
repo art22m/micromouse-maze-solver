d21494500f1c1c35ed8a54f91d526ae572c9da7b
Artem Murashko <art22m@yandex-team.ru>
2024-10-27 17:35:35 +0300

    dummy flag

--- cmd/main.go
+++ cmd/main.go
@@ -16,15 +16,21 @@ const (
 )
 
 func main() {
+	dummy := flag.Bool("dummy", false, "")
 	backward := flag.Bool("bw", false, "")
 	sip := flag.String("sip", sensorsIP, "")
 	mip := flag.String("bip", motorsIP, "")
 	id := flag.String("id", robotID, "")
 	flag.Parse()
-	fmt.Println(*sip, *mip, *id)
+	fmt.Println(*sip, *mip, *id, dummy, backward)
 
-	//mover := mo.NewDummyMover(*sip, *mip, *id)
-	mover := mo.NewSmartMover(*sip, *mip, *id)
+	var mover mo.Mover
+	if *dummy {
+		mover = mo.NewDummyMover(*sip, *mip, *id)
+	} else {
+		mover = mo.NewSmartMover(*sip, *mip, *id)
+	}
+
 	config := solver.FloodFillConfig{
 		StartDirection:  maze.Up,
 		StartPosition:   solver.NewPosition(0, 0),
