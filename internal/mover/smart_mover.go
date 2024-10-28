1b49e5bb38046f2540cee044fb431f3099a9c177
5ur3 <an.sandimirov@gmail.com>
2024-10-28 19:11:29 +0300

    DFSJKLjadsklf

--- internal/mover/smart_mover.go
+++ internal/mover/smart_mover.go
@@ -143,7 +143,7 @@ func (m *SmartMover) Forward(cell int) {
 			m.centering()
 		}
 
-		m.move("forward", 170)
+		m.move("forward", 120)
 
 		dist := m.calcFrontDistance()
 		fmt.Println("!!! dist:", dist)
